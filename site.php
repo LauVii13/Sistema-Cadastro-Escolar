97450a50672449a4614de3f59e83cdd42b357ace
LauVii13 <viniciusglaureano@gmail.com>
2023-03-22 15:56:54 -0300

    Preparing to implement sketched design in Figma

--- site.php
+++ site.php
@@ -1,17 +1,25 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="pt-br">
 
 <head>
   <meta charset="UTF-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Document</title>
+
+  <!--Font Awesome-->
+  <link rel="stylesheet" href="https://cdn.es.gov.br/fonts/font-awesome/css/font-awesome.min.css">
+
+  <link rel="stylesheet" href="./areaDeTrabalho/stylesheets/style.css">
+
+
+  <title>Sistema</title>
 
 
   <?php
+  ini_set('session.save_path', realpath(dirname($_SERVER['DOCUMENT_ROOT']) . '/../tmp'));
   session_start();
   if ((!isset($_SESSION['login']) == true) and (!isset($_SESSION['senha']) == true)) {
-    header('location:../index.php');
+    header('location:index.html');
   }
   $logado = $_SESSION['login'];
   ?>
